53a17e445b84c412cf83391254e22c3c16341bf1
hannahdeleon21 <lorrainehannahdeleon@gmail.com>
2025-04-30 08:33:28 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -28,8 +28,4 @@ export default defineConfig({
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
-  server:{
-    host: '0.0.0.0',
-    port: 5000
-  }'
 });
